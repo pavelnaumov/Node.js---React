8045ea61120fff70bf190bbdb285d3185329ffa3
Paul Naumov <pavliknaumov@MacBook-Pro-Pavel-Naumov.local>
2020-04-30 23:20:57 +0300

    small change

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ port: 5000 });
-});
-
 /**
  * Using dynamic port in production
- * 
+ *
  * Using port 5000 for development
  */
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send({ port: PORT });
+});
 
 app.listen(PORT);
