3a55d33920bcf6fd931a7835b1947a06dfbfe08d
Paul Naumov <pavliknaumov@MacBook-Pro-Pavel-Naumov.local>
2020-05-05 00:24:44 +0300

    feat: add config for client depployment

--- server/index.js
+++ server/index.js
@@ -24,5 +24,21 @@ app.use(passport.session());
 require("./routes/authRoutes")(app); // auth routes
 require("./routes/billingRoutes")(app); // billing routes
 
+/**
+ * Node Configuration for Production
+ */
+
+if (process.env.NODE_ENV === "production") {
+  // 1. Telling Node to serve the production assets
+  // 2. Telling Express to serve index.html if the routes aren't recognized (Express vs React routes)
+
+  app.use(express.static("client/build")); // 1
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // 2
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
